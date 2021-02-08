2249705d3a043b17498d6038e71d6ca2b903a143
Aruzhan Ibraiym <190103192@stu.sdu.edu.kz>
2021-02-08 17:21:23 +0600

    secound commit

---
+++ Variblesss.java
@@ -0,0 +1,19 @@
+public class Varibles{
+	public static void main(String[] args){
+		int myInt = 575;
+		short myShort = 3221;
+		long myLong = 12345678;
+
+		double myDouble = 234.4;
+		float myFloat = 23432f;
+
+		char c = 'a';
+		boolean a = true;
+
+		byte myByte = 100;
+
+
+
+		System.out.print();
+	}
+}
